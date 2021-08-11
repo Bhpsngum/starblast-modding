6a08e7fa84f9b37a0ac2105ec63b9cf104a08d48
Bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2021-08-11 14:29:57 +0900

    switching the mathematical operators with the binary one

--- src/utils/convertStats.js
+++ src/utils/convertStats.js
@@ -4,8 +4,8 @@ module.exports = function(data) {
   if (isNaN(data)) return 0;
   let stats = [];
   for (let i = 0; i < 8; i++) {
-    stats.push(data % 16); // equivalent to 15&data
-    data = data / 16 // equivalent to data = data >> 4
+    stats.push(15 & data); // equivalent to data % 16
+    data = data >> 4 // equivalent to data = data / 16
   }
   return stats
 }
