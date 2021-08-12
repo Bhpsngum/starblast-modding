847803f77457acd8c9a160a4f54036d79e03aaa0
Bhpsngum <bhpsngumtrongwikipediatiengvie@gmail.com>
2021-08-13 02:51:50 +0900

    favor `ObjectManager.set` method like the old Modding Client, `ObjectManager.add` will only stays as an alias

--- src/managers/ObjectManager.js
+++ src/managers/ObjectManager.js
@@ -3,6 +3,13 @@
 const StructureManager = require("./StructureManager.js");
 const ObjectTypeManager = require("./ObjectTypeManager.js");
 const Object3D = require("../structures/Object.js");
+const setObject = function (data) {
+  let object = this.create(data);
+  let fObject = this.findById(object.id, true);
+  if (fObject == null) this.insert(object);
+  else object = fObject;
+  object.set(data)
+}
 
 class ObjectManager extends StructureManager {
   constructor(game) {
@@ -10,12 +17,12 @@ class ObjectManager extends StructureManager {
     this.types = new ObjectTypeManager(game);
   }
 
-  add (data) {
-    let object = this.create(data);
-    let fObject = this.findById(object.id, true);
-    if (fObject == null) this.insert(object);
-    else object = fObject;
-    object.set(data)
+  set (data) {
+    return setObject.call(this, data)
+  }
+
+  add (data) { // placeholder, same functionality of 'ObjectManager.set'
+    return setObject.call(this, data)
   }
 
   update () {
