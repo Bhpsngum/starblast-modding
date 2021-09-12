7e3fc4514847d5cdff3f68e3bf2f31df4e0a31fc
Bhpsngum <bhpsngumproductions@gmail.com>
2021-09-12 12:43:36 +0700

    better update for `toString` function

--- src/utils/toString.js
+++ src/utils/toString.js
@@ -1,7 +1,5 @@
 'use strict';
 
 module.exports = function (value) {
-  let temp = {};
-  if (value == null) return (temp[value] = 1, Object.keys(temp)[0]);
-  try { return value.toString() } catch (e) { return "" }
+  try { return String(value) } catch (e) { return "" }
 }
